c97184542e8e1be02be64ee4d85a84e8d0685090
zBinFinn <ermert.finn@icloud.com>
2024-11-25 16:12:20 +0100

    Added literals in the form of str[hi] for lots of types

--- src/main/java/org/example/Argument.java
+++ src/main/java/org/example/Argument.java
@@ -7,23 +7,128 @@ public class Argument {
     String value;
     String scope;
     String[] vector = new String[3];
+    String[] location = new String[5];
+    String[] sound;
+    String[] particle = new String[2];
+
+    private String parseFromUntil (String input, String start, String end) {
+        if (!input.endsWith(end)) {
+            ErrUtil.sendError(input + " did not end with " + end);
+            return "ERROR: Input did not end with expected end";
+        }
+        input = input.replaceFirst(start, "");
+
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < input.length() - end.length(); i++) {
+            out.append(input.charAt(i));
+        }
+
+        return out.toString();
+    }
 
     public Argument (String arg) {
+        // Explicitly typed arguments
+        // Number
+        if (arg.startsWith("num[")) {
+            type = Type.NUMBER;
+            value = parseFromUntil(arg, "num\\[", "]");
+            return;
+        }
+        // String
+        if (arg.startsWith("str[")) {
+            type = Type.STRING;
+            value = parseFromUntil(arg, "str\\[", "]");
+            return;
+        }
+        // Text
+        if (arg.startsWith("txt[")) {
+            type = Type.TEXT;
+            value = parseFromUntil(arg, "txt\\[", "]");
+            return;
+        }
+        // Vector
+        if (arg.startsWith("vec[")) {
+            type = Type.VECTOR;
+            value = parseFromUntil(arg, "vec\\[", "]");
+            vector = value.split(" ");
+            return;
+        }
+        // Location
+        if (arg.startsWith("loc[")) {
+            type = Type.LOCATION;
+            value = parseFromUntil(arg, "loc\\[", "]");
+            location = value.split(" ");
+            return;
+        }
+        // Sound
+        if (arg.startsWith("snd[")) {
+            type = Type.SOUND;
+            value = parseFromUntil(arg, "snd\\[", "]");
+            sound = value.split(" ");
+            return;
+        }
+        // Particle
+        if (arg.startsWith("part[")) {
+            type = Type.PARTICLE;
+            value = parseFromUntil(arg, "part\\[", "]");
+            particle = value.split(" ");
+            particle[1] = particle[1].replaceAll(";", ",");
+            return;
+        }
+
+        // Variable
+        if (arg.startsWith("var[")) {
+            type = Type.VARIABLE;
+            String parsed = parseFromUntil(arg, "var\\[", "]");
+
+            StringBuilder scopeBuilder = new StringBuilder();
+            StringBuilder valueBuilder = new StringBuilder();
+            boolean scope = true;
+            for (char c : parsed.toCharArray()) {
+                if (c == ' ') {
+                    if (!scope) {
+                        valueBuilder.append(c);
+                    }
+                    scope = false;
+                } else {
+                    if (scope) {
+                        scopeBuilder.append(c);
+                    } else {
+                        valueBuilder.append(c);
+                    }
+                }
+            }
+
+            value = valueBuilder.toString();
+            this.scope = scopeBuilder.toString();
+            return;
+        }
+
+
+
+
+
+
+        // Implicitly typed arguments
+        // Number
         if (CharHelper.numeric(arg.charAt(0))) {
             type = Type.NUMBER;
             value = arg;
             return;
         }
+        // String
         if (arg.charAt(0) == '"') {
             type = Type.STRING;
             value = arg.replaceAll("\"", "");
             return;
         }
+        // Text
         if (arg.charAt(0) == '\'') {
             type = Type.TEXT;
             value = arg.replaceAll("'", "");
             return;
         }
+        // Variable
         if (CharHelper.alphabetic(arg.charAt(0))) {
             type = Type.VARIABLE;
             StringBuilder scopeBuilder = new StringBuilder();
@@ -41,6 +146,7 @@ public class Argument {
             value = arg;
             return;
         }
+        // Vector
         if (arg.charAt(0) == '<') {
             type = Type.VECTOR;
             int charIndex = 1;
@@ -68,23 +174,41 @@ public class Argument {
     }
 
     private String getScope() {
-        switch (scope) {
-            case "LOCAL": return "local";
-            case "GAME": return "unsaved";
-            case "SAVE": return "saved";
-            case "LINE":
-            default: return "line";
-        }
+        return switch (scope) {
+            case "LOCAL" -> "local";
+            case "GAME" -> "unsaved";
+            case "SAVE" -> "saved";
+            default -> "line";
+        };
     }
 
     public String toString(int slot) {
+        return "{\"item\":{\"id\":\"" + type.val + "\",\"data\":{" + getData() + "}},\"slot\":" + slot + "}";
+        /*
         if (type == Type.VARIABLE) {
             return "{\"item\":{\"id\":\"" + type.val + "\",\"data\":{\"name\":\"" + value + "\",\"scope\":\"" + getScope() + "\"}},\"slot\":" + slot + "}";
         }
         if (type == Type.VECTOR) {
             return "{\"item\":{\"id\":\"" + type.val + "\",\"data\":{\"x\":" + vector[0] + ",\"y\":" + vector[1] + ",\"z\":" + vector[2] + "}},\"slot\":" + slot + "}";
         }
-        return "{\"item\":{\"id\":\"" + type.val + "\",\"data\":{\"name\":\"" + value + "\"}},\"slot\":" + slot + "}";
+        if (type == Type.LOCATION) {
+            return "{\"item\":{\"id\":\"" + type.val + "\",\"data\":{\"isBlock\":" + "false" + ",\"loc\":{\"x\":" + location[0] + ",\"y\":" + location[1] + ",\"z\":" + location[2] + ",\"pitch\":" + location[3] + ",\"yaw\":" + location[4] + "}}},\"slot\":" + slot + "}";
+        }
+        return "{\"item\":{\"id\":\"" + type.val + "\",\"data\":{\"name\":\"" + value + "\"}},\"slot\":" + slot + "}"; */
+    }
+
+    private String getData() {
+        return switch (type) {
+            case VARIABLE -> "\"name\":\"" + value + "\",\"scope\":\"" + getScope() + "\"";
+            case STRING, TEXT, NUMBER -> "\"name\":\"" + value + "\"";
+            case VECTOR -> "\"x\":" + vector[0] + ",\"y\":" + vector[1] + ",\"z\":" + vector[2];
+            case LOCATION -> "\"isBlock\":" + "false" + ",\"loc\":{\"x\":" + location[0] + ",\"y\":" + location[1] + ",\"z\":" + location[2] + ",\"pitch\":" + location[3] + ",\"yaw\":" + location[4] + "}";
+            case SOUND -> "\"pitch\":" + sound[0] + ",\"vol\":" + sound[1] + ",\"sound\":\"" + sound[2] + "\"" + ((sound.length == 3) ? "" : ",\"variant\":\"" + sound[3] + "\"");
+            case PARTICLE -> "\"particle\":\"" + particle[0] + "\",\"cluster\":{" + particle[1] + "}";
+            default -> "NULL";
+        };
+
+
     }
 
 //{"items":[{"item":{"id":"num","data":{"name":"2"}},"slot":0},{"item":{"id":"num","data":{"name":"15"}},"slot":1}]}
@@ -97,7 +221,10 @@ public class Argument {
         NUMBER ("num"),
         TEXT ("comp"),
         VARIABLE("var"),
-        VECTOR ("vec");
+        VECTOR ("vec"),
+        LOCATION ("loc"),
+        PARTICLE("part"),
+        SOUND ("snd");
 
         final String val;
         Type (String s) {
